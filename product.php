f9d9cd4203bc55f0662b9dfc9a983fe2c5b4373b
Thomas Michiels <thomasmichiels0@gmail.com>
2023-10-17 09:44:51 +0200

    kleine aanpassing product.php

--- product.php
+++ product.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $_GET["name"]; ?></title>
+    <title>Productnaam</title>
     <script src="js/readmore.js"></script>
     <link href="src/styles.css" rel="stylesheet">
     <link href="src/header.css" rel="stylesheet">
