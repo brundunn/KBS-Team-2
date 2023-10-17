7879bfa1e9240808fbdf6b897ec2328b0023631b
Brandon <brandon99@live.nl>
2023-10-17 11:12:41 +0200

    product-raster.css updated

--- product-raster.php
+++ product-raster.php
@@ -1,2 +1,20 @@
+<link rel="stylesheet" href="src/product-raster.css">
+<div class="product-raster">
 <?php
-    $connection = mysqli_connect('127.0.0.1', )
+    $connection = mysqli_connect('127.0.0.1','root', '','nerdy_gadgets_start', '3306');
+
+
+    ?>
+    <div class="raster-item">aaaaaaaaaaaaaaaa</div>
+    <div class="raster-item">aaaaaaaaaaaaaaaa</div>
+    <div class="raster-item">aaaaaaaaaaaaaaaa</div>
+    <div class="raster-item">aaaaaaaaaaaaaaaa</div>
+    <div class="raster-item">aaaaaaaaaaaaaaaa</div>
+    <div class="raster-item">aaaaaaaaaaaaaaaa</div>
+    <div class="raster-item">aaaaaaaaaaaaaaaa</div>
+    <div class="raster-item">aaaaaaaaaaaaaaaa</div>
+    <div class="raster-item">aaaaaaaaaaaaaaaa</div>
+
+
+
+</div>
