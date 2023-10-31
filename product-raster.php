acc12528fade51d72a992d11ae329b35bae2ac3f
hirakib <brandon.hiraki@hva.nl>
2023-10-31 09:04:15 +0100

    product-raster.php updated

--- product-raster.php
+++ product-raster.php
@@ -2,7 +2,7 @@
 <div class="product-raster">
     <?php
     $connection = mysqli_connect('127.0.0.1','root', '','nerdy_gadgets_start', '3306');
-    $sql_selectALL = "SELECT * FROM product LIMIT 2";
+    $sql_selectALL = "SELECT * FROM product";
     $res = mysqli_query($connection,$sql_selectALL);
 
     while($row = mysqli_fetch_assoc($res)){
@@ -12,13 +12,14 @@
         $productDesc = $row['description'];
         $productPrice = $row['price'];
 
-        echo "<a href='product.php?id=$productID'
-                <div class='raster-item'>
-                <div class='raster-img'><img src ='img/product_images/$productImage.jpg'></div>
-                <div class='raster-name'>$productName</div>
-                <div class='raster-price'>€$productPrice</div>
-                </div>
-                </a>";
+        echo "<a href='product.php?id=$productID'>";
+        echo "<div class='raster-item'>";
+        echo "<div class='raster-img'><img src ='img/product_images/$productImage.jpg'></div>";
+        echo "<div class='raster-name'>$productName</div>";
+        echo "<div class='raster-price'>€$productPrice</div>";
+        echo "</div>";
+        echo "</a>";
+
     }
     mysqli_close($connection);
     ?>
