21d0bb27f195266dd09591e16af1d843f828e383
Thomas Michiels <thomasmichiels0@gmail.com>
2023-11-17 22:18:35 +0100

    samenhang gecreëerd tussen uitgelichte producten op homepage en product assortiment page (visueel en in de code)

--- product-overzicht.php
+++ product-overzicht.php
@@ -9,8 +9,8 @@
     <script src="js/readmore.js"></script>
     <link href="src/styles.css" rel="stylesheet">
     <link href="src/header.css" rel="stylesheet">
-    <link href="src/Assortiment.css" rel="stylesheet">
-
+    <link href="src/product-overzicht.css" rel="stylesheet">
+    <link href="src/product-raster.css" rel="stylesheet">
 
 </head>
 <body>
@@ -43,13 +43,12 @@
         praesentium, recusandae saepe ullam! A accusantium architecto aspernatur excepturi fugiat molestias obcaecati
         sequi similique voluptatem voluptatum?</p>
 
-    <div class="sidenav">
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Clients</a>
-        <a href="#">Contact</a>
-    </div>
-    <div class="Assortment-products">
+        <div class="sidenav">
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+        </div>
 
     <!-- DATABASE CONNECTIE -->
     <?php
@@ -73,26 +72,41 @@
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        // output data of each row
-        while ($row = $result->fetch_assoc()) {
-            include "product-raster.php";
-//            echo "<a href='product.php?id=" . $row["id"] .
-//                "'>id: " . $row["id"] .
-//                " <br> name: " . $row["name"] .
-//                " <br> description: " . $row["description"] .
-//                " <br> price: " . $row["price"] .
-//                " <br> category: " . $row["category"] .
-//                " <br> <img src=\"img/product_images/" . $row["image"] . ".jpg\" alt=\"" . $row["name"] . "\">" .
-//                "</a>" .
-//                "<br><br>";
+        // PRODUCT RASTER
+        echo '<div class="product-raster">';
+        $connection = mysqli_connect('127.0.0.1', 'root', '', 'nerdy_gadgets_start', '3306');
+        $sql_selectALL = "SELECT * FROM product";
+        $res = mysqli_query($connection, $sql_selectALL);
+
+        while ($row = mysqli_fetch_assoc($res)) {
+            $productID = $row["id"];
+            $productImage = $row["image"];
+            $productName = $row["name"];
+            $productDesc = $row['description'];
+            $productPrice = $row['price'];
+
+            echo "<a href='product.php?id=$productID'>";
+            echo "<div class='raster-item'>";
+            echo "<div class='raster-img'><img src ='img/product_images/$productImage.jpg'></div>";
+            echo "<h3 class='raster-name'>$productName</h3>";
+            echo "<div class='raster-price-and-link-container'>";
+            echo "<p class='raster-price'>€$productPrice</p>";
+            echo '
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"> 
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"></path> 
+                        </svg> 
+             ';
+            echo "</div>";
+            echo "</div>";
+            echo "</a>";
         }
+        mysqli_close($connection);
+        echo "</div>";
     } else {
         echo "0 results";
     }
     $conn->close();
-     ?>
+    ?>
 
-
-</div>
 </body>
 </html>
--- product-raster.php
+++ product-raster.php
@@ -1,27 +1,27 @@
-<link rel="stylesheet" href="src/product-raster.css">
-<div class="product-raster">
-    <?php
-    $connection = mysqli_connect('127.0.0.1','root', '','nerdy_gadgets_start', '3306');
-//    $sql_selectALL = "SELECT * FROM product LIMIT 20";
-    $sql_selectALL = "SELECT * FROM product";
-    $res = mysqli_query($connection,$sql_selectALL);
-
-    while($row = mysqli_fetch_assoc($res)){
-        $productID = $row["id"];
-        $productImage = $row["image"];
-        $productName = $row["name"];
-        $productDesc = $row['description'];
-        $productPrice = $row['price'];
-
-        echo "<a href='product.php?id=$productID'>";
-        echo "<div class='raster-item'>";
-        echo "<div class='raster-img'><img src ='img/product_images/$productImage.jpg'></div>";
-        echo "<div class='raster-name'>$productName</div>";
-        echo "<div class='raster-price'>€$productPrice</div>";
-        echo "</div>";
-        echo "</a>";
-
-    }
-    mysqli_close($connection);
-    ?>
-</div>
+<!--<link rel="stylesheet" href="src/product-raster.css">-->
+<!--<div class="product-raster">-->
+<!--    --><?php
+//    $connection = mysqli_connect('127.0.0.1','root', '','nerdy_gadgets_start', '3306');
+////    $sql_selectALL = "SELECT * FROM product LIMIT 20";
+//    $sql_selectALL = "SELECT * FROM product";
+//    $res = mysqli_query($connection,$sql_selectALL);
+//
+//    while($row = mysqli_fetch_assoc($res)){
+//        $productID = $row["id"];
+//        $productImage = $row["image"];
+//        $productName = $row["name"];
+//        $productDesc = $row['description'];
+//        $productPrice = $row['price'];
+//
+//        echo "<a href='product.php?id=$productID'>";
+//        echo "<div class='raster-item'>";
+//        echo "<div class='raster-img'><img src ='img/product_images/$productImage.jpg'></div>";
+//        echo "<div class='raster-name'>$productName</div>";
+//        echo "<div class='raster-price'>€$productPrice</div>";
+//        echo "</div>";
+//        echo "</a>";
+//
+//    }
+//    mysqli_close($connection);
+//    ?>
+<!--</div>-->
