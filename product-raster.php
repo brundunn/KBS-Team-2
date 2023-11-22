163941c23a4e21a4fc776a8bb761450cd5b583af
Thomas Michiels <thomasmichiels0@gmail.com>
2023-11-22 02:44:54 +0100

    kleine aanpassing

--- product-raster.php
+++ product-raster.php
@@ -15,7 +15,6 @@ function toonProductRaster($query) {
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    // echo "Connected successfully<br>";
 
 
     // QUERY
