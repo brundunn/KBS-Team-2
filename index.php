99167c1bf43876c5554753d8032040e1631bd17d
Thomas Michiels <thomasmichiels0@gmail.com>
2023-11-24 10:53:48 +0100

    banner gefixt

--- index.php
+++ index.php
@@ -37,41 +37,42 @@
         ?>
     </ul>
     <hr>
+    <!-- Dynamische HTML Slider --->
+
+    <div class="slideshow-container">
+
+        <div class="mySlides fade">
+            <div class="numbertext">1 / 3</div>
+            <img src="img/slide1.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 3</div>
+            <img src="img/slide2.jpg" style="width:100%">
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 3</div>
+            <img src="img/slide3.jpg" style="width:100%">
+        </div>
+
+        <a class="prev" onclick="plusSlides(-1)">❮</a>
+        <a class="next" onclick="plusSlides(1)">❯</a>
+
+    </div>
+    <br>
+
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+    </div>
+
+    <!-- Dynamische HTML Slider --->
     <div class="brand-introduction-container">
-        <div class="empty">
+        <div class="empty"></div>
 
-           <!-- Dynamische HTML Slider --->
 
-            <div class="slideshow-container">
-
-                <div class="mySlides fade">
-                    <div class="numbertext">1 / 3</div>
-                    <img src="img/slide1.jpg" style="width:100%">
-                </div>
-
-                <div class="mySlides fade">
-                    <div class="numbertext">2 / 3</div>
-                    <img src="img/slide2.jpg" style="width:100%">
-                </div>
-
-                <div class="mySlides fade">
-                    <div class="numbertext">3 / 3</div>
-                    <img src="img/slide3.jpg" style="width:100%">
-                </div>
-
-                <a class="prev" onclick="plusSlides(-1)">❮</a>
-                <a class="next" onclick="plusSlides(1)">❯</a>
-
-            </div>
-            <br>
-
-            <div style="text-align:center">
-                <span class="dot" onclick="currentSlide(1)"></span>
-                <span class="dot" onclick="currentSlide(2)"></span>
-                <span class="dot" onclick="currentSlide(3)"></span>
-            </div>
-
-            <!-- Dynamische HTML Slider --->
 
         <div class="hero">
             <img class="intro-logo" src="img/Logo_KBS-removebg-preview.png" alt="logo">
