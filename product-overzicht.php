ee05dd33a2ecbabad41e5c5bf9e12ebb626096fd
Brandon <brandon99@live.nl>
2023-11-28 13:07:42 +0100

    frontend filter

--- product-overzicht.php
+++ product-overzicht.php
@@ -30,14 +30,15 @@
     <!-- Einde breadcrumbs -->
 
     <h1>Assortiment</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet aspernatur atque esse molestiae
-        praesentium, recusandae saepe ullam! A accusantium architecto aspernatur excepturi fugiat molestias obcaecati
-        sequi similique voluptatem voluptatum?</p>
+    <p>Welkom bij ons uitgebreide assortiment, waar kwaliteit, diversiteit en innovatie samenkomen om aan al jouw behoeften te voldoen.
+        Ontdek een wereld van mogelijkheden terwijl je bladert door ons zorgvuldig samengestelde aanbod, ontworpen om aan de uiteenlopende
+        wensen van onze gewaardeerde klanten te voldoen.</p>
 
-        <div class="sidenav">
-            <h3>Filter op:</h3><br>
-            <h4>Categorie</h4>
-            <form action="" method="post">
+    <div class="sidenav-raster-container">
+    <div class="sidenav">
+        <h3>Filter op:</h3><br>
+        <h4>Categorie</h4>
+        <form action="" method="post">
             <input type="checkbox" name="category[]" value="laptops" <?php
             if (isset($_POST['category'])) {
                 $category = $_POST["category"];
@@ -90,7 +91,7 @@
                     echo 'checked';
                 }
             }
-            
+
             ?>>
             <label for="category">Componenten</label>
             <br>
@@ -107,46 +108,62 @@
             <br>
             <h4>Prijsklasse</h4>
             <h4>Populariteit</h4>
-            <input type="submit" value="Submit">
-            </form>
-            <?php
-            if (isset($_POST["category"])) {
-                print_r($_POST["category"]);
+            <input id="submit" type="submit" value="Submit">
+        </form>
+        <?php
+        $query = "SELECT * FROM product";
+
+        if (isset($_POST["category"])) {
+            $query = $query . " WHERE ";
+//            print_r($_POST["category"]);
+            $res = $_POST["category"];
+            $len = sizeof($res);
+//            echo '<br>'.$len;
+            if ($len == 1) {
+                foreach ($res as $category) {
+                    $query = $query . 'category = "' . $category . '"';
+                }
+            } else {
+                foreach ($res as $i => $category) {
+                    if ($i != $len-1) {
+                        $query = $query . 'category = "' . $category . '" OR ';
+                    } else {
+                        $query = $query . 'category = "' . $category . '"';
+                    }
+
+                }
             }
-            ?>
+            $query = $query . ';';
+//echo '<br>' . $query;
+//                $query = 'SELECT * FROM product WHERE category = "laptops"';
+        }
 
-            <?php
-            // DATABASE CONNECTIE
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "nerdy_gadgets_start";
+        ?>
 
-            // Create connection
-            $conn = new mysqli($servername, $username, $password, $dbname); // Connect direct met de database ipv alleen met SQL
-            // Check connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
+        <?php
+        // DATABASE CONNECTIE
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "nerdy_gadgets_start";
 
-
-
-
-
-            ?>
-<!--            <a href="#">About</a>-->
-<!--            <a href="#">Services</a>-->
-<!--            <a href="#">Clients</a>-->
-<!--            <a href="#">Contact</a>-->
-        </div>
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname); // Connect direct met de database ipv alleen met SQL
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        ?>
+        <!--            <a href="#">About</a>-->
+        <!--            <a href="#">Services</a>-->
+        <!--            <a href="#">Clients</a>-->
+        <!--            <a href="#">Contact</a>-->
+    </div>
 
     <?php
-    $query = "";
-
-    $query = $query . "WHERE ...";
     include 'product-raster.php';
-    toonProductRaster("SELECT * FROM product");
+    toonProductRaster("$query");
     ?>
-
+    </div>
 </body>
 </html>
