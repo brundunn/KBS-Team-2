752a486fc7391852e891dd427b6f4fc3b51c0ad2
mauri <maurice.reijntjes@gmail.com>
2023-12-01 12:05:29 +0100

    uitgelichte producten onderaan pagina

--- product.php
+++ product.php
@@ -34,7 +34,7 @@ if ($result->num_rows > 0) {
         $description = $row["description"];
         $price = $row["price"];
         $category = $row["category"];
-        $imgSrc = "img/product_images/" . $row["image"]  . ".jpg";
+        $imgSrc = "img/product_images/" . $row["image"] . ".jpg";
 
         // Get products from the same category
         $sameCategorySql = "SELECT * FROM product WHERE category='$category' AND id!=$id";
@@ -85,55 +85,40 @@ $conn->close();
     </ul>
     <hr>
     <!-- Einde breadcrumbs -->
-    <h2><?php echo $name ?></h2>
-    <h3><?php echo "Prijs: €$price" ?></h3>
-    <h4><?php echo "Categorie: $category"?></h4>
-    <p><?php echo $description ?></p>
-    <img src="<?php echo $imgSrc ?>" alt="<?php echo $name?>">
 
 
-</div>
 
-<div class="product-list">
-    <h2>Misschien bent u ook geïntereseerd in::</h2>
-    <?php foreach ($sameCategoryProducts as $product) { ?>
-        <div class="product-item">
-            <a href="product.php?id=<?php echo $product['id']; ?>">
-                <img src="img/product_images/<?php echo $product['image']; ?>.jpg" alt="<?php echo $product['name']; ?>">
-                <h5><?php echo $product['name']; ?></h5>
-            </a>
-        </div>
-    <?php } ?>
 
-    <!-- Begin productpagina -->
-    <div class="product-informatie">
-        <h2><?php echo $name ?></h2>
-        <?php
-        include 'src/review-functions.php';
-        // Toon gemiddelde score van het product, zonder het totaal aantal reviews
-        gemiddeldeScoreZonderTotaal("SELECT AVG(score) AS avgScore
+
+        <!-- Begin productpagina -->
+        <div class="product-informatie">
+            <h2><?php echo $name ?></h2>
+            <?php
+            include 'src/review-functions.php';
+            // Toon gemiddelde score van het product, zonder het totaal aantal reviews
+            gemiddeldeScoreZonderTotaal("SELECT AVG(score) AS avgScore
 FROM product_review WHERE product_id = " . $id, "SELECT COUNT(*) AS amountOfReviews
 FROM product_review WHERE product_id = " . $id);
-        ?>
-        <h3><?php echo "Prijs: €$price" ?></h3>  <!-- prijs -->
-        <h4><?php echo "Categorie: $category" ?></h4> <!-- categorie -->
-        <p><?php echo $description ?></p> <!-- beschrijving -->
-        <img src="<?php echo $imgSrc ?>" alt="<?php echo $name ?>"> <!-- afbeelding -->
-    </div>
+            ?>
+            <h3><?php echo "Prijs: €$price" ?></h3>  <!-- prijs -->
+            <h4><?php echo "Categorie: $category" ?></h4> <!-- categorie -->
+            <p><?php echo $description ?></p> <!-- beschrijving -->
+            <img src="<?php echo $imgSrc ?>" alt="<?php echo $name ?>"> <!-- afbeelding -->
+        </div>
 
-    <?php
-    if (isset($_COOKIE["shopping_cart"])) {
-        $cookie_data = stripslashes($_COOKIE["shopping_cart"]);
-        $cart_data = json_decode($cookie_data, true);
-    } else {
-        $cart_data = array();
-    }
+        <?php
+        if (isset($_COOKIE["shopping_cart"])) {
+            $cookie_data = stripslashes($_COOKIE["shopping_cart"]);
+            $cart_data = json_decode($cookie_data, true);
+        } else {
+            $cart_data = array();
+        }
 
-    $item_id_list = array_column($cart_data, 'product_id');
+        $item_id_list = array_column($cart_data, 'product_id');
 
 
-    if (in_array($id, $item_id_list)) { // Als het product in de winkelwagen zit, toon dat dan
-        echo '<button class="add-product-to-cart-button product-in-cart">
+        if (in_array($id, $item_id_list)) { // Als het product in de winkelwagen zit, toon dat dan
+            echo '<button class="add-product-to-cart-button product-in-cart">
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="">
   <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
 </svg>
@@ -146,9 +131,9 @@ FROM product_review WHERE product_id = " . $id);
                     <p>In winkelwagen</p></button>';
 
 
-    } else {
-        // Product toevoegen aan winkelwagen - button
-        echo '<form method="post" action="">
+        } else {
+            // Product toevoegen aan winkelwagen - button
+            echo '<form method="post" action="">
         <input type="hidden" name="quantity" value="1">
         <input type="hidden" name="product_id" value="' . $id . '">
                 <button type="submit" name="add_to_cart" value="1" class="add-product-to-cart-button">
@@ -165,92 +150,104 @@ FROM product_review WHERE product_id = " . $id);
                     <p>In winkelwagen</p>
                 </button>
     </form>';
-    }
-    ?>
+        }
+        ?>
+    <div class="product-list">
+        <h2>Misschien bent u ook geïntereseerd in::</h2>
+        <?php foreach ($sameCategoryProducts as $product) { ?>
+        <div class="product-item">
+            <a href="product.php?id=<?php echo $product['id']; ?>">
+                <img src="img/product_images/<?php echo $product['image']; ?>.jpg"
+                     alt="<?php echo $product['name']; ?>">
+                <h5><?php echo $product['name']; ?></h5>
+            </a>
+        </div>
+    <?php } ?>
 
-    <br><br>
-    <hr>
-    <!-- Product review -->
-    <h3>Reviews over <?php echo $name ?></h3>
-    <?php
-    echo '<a href="write-review.php?type=product&id=' . $id . '">Schrijf review</a>';
+        <br><br>
+        <hr>
+        <!-- Product review -->
+        <h3>Reviews over <?php echo $name ?></h3>
+        <?php
+        echo '<a href="write-review.php?type=product&id=' . $id . '">Schrijf review</a>';
 
-    gemiddeldeScore("SELECT AVG(score) AS avgScore
+        gemiddeldeScore("SELECT AVG(score) AS avgScore
 FROM product_review WHERE product_id = " . $id, "SELECT COUNT(*) AS amountOfReviews
 FROM product_review WHERE product_id = " . $id);
 
-    echo '<br>';
+        echo '<br>';
 
-    //    include 'src/print-star-functions.php';
+        //    include 'src/print-star-functions.php';
 
-    // DATABASE CONNECTIE
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "nerdy_gadgets_start";
+        // DATABASE CONNECTIE
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "nerdy_gadgets_start";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname); // Connect direct met de database ipv alleen met SQL
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    // echo "Connected successfully<br>";
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname); // Connect direct met de database ipv alleen met SQL
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        // echo "Connected successfully<br>";
 
-    // QUERY - haal alle product reviews op
-    $sql = "SELECT pr.product_id, u.first_name, u.surname_prefix, u.surname, pr.date, pr.score, pr.description
+        // QUERY - haal alle product reviews op
+        $sql = "SELECT pr.product_id, u.first_name, u.surname_prefix, u.surname, pr.date, pr.score, pr.description
 FROM product_review pr
 JOIN user u ON pr.user_id = u.id
 WHERE pr.product_id = " . $id . "
 ORDER BY date DESC;";
-    // RESULT
-    $result = $conn->query($sql);
+        // RESULT
+        $result = $conn->query($sql);
 
-    $ingekort = false;
+        $ingekort = false;
 
-    if ($result->num_rows > 0) { // checken of er product reviews zijn voor het product
-        if ($result->num_rows > 4) { // als er meer dan 4 reviews zijn, gaan we het inkorten
+        if ($result->num_rows > 0) { // checken of er product reviews zijn voor het product
+            if ($result->num_rows > 4) { // als er meer dan 4 reviews zijn, gaan we het inkorten
 // QUERY - nu ingekort
-            $sql = "SELECT pr.product_id, u.first_name, u.surname_prefix, u.surname, pr.date, pr.score, pr.description
+                $sql = "SELECT pr.product_id, u.first_name, u.surname_prefix, u.surname, pr.date, pr.score, pr.description
 FROM product_review pr
 JOIN user u ON pr.user_id = u.id
 WHERE pr.product_id = " . $id . "
 ORDER BY date DESC LIMIT 4;";
-            // RESULT
-            $result = $conn->query($sql);
-            $ingekort = true;
-        }
-
-        // output data of each row
-        while ($row = $result->fetch_assoc()) {
-            $date = substr($row["date"], 0, -3);
-
-            echo '<div class="review highlighted-review">';
-            echo printStars($row["score"]) .
-                "<h3>" . $row["first_name"];
-            if (!empty($row["surname_prefix"])) { // check of persoon een tussenvoegsel heeft
-                echo " " . $row["surname_prefix"];
+                // RESULT
+                $result = $conn->query($sql);
+                $ingekort = true;
             }
-            echo " " . $row["surname"] . "</h3>" .
-                "<h4>" . $date . '</h4>';
-            if (!empty($row["description"])) { // check of persoon een beschrijving heeft geplaatst bji de review
-                echo "<p>" . $row["description"] . '</p>';
-            }
-            echo '</div>';
-            echo "<br>";
 
-        }
-        if ($ingekort) {
-            echo '<a class="reviews-ingekort" href="product-reviews.php?id=' . $id . '">Bekijk alle reviews<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                $date = substr($row["date"], 0, -3);
+
+                echo '<div class="review highlighted-review">';
+                echo printStars($row["score"]) .
+                    "<h3>" . $row["first_name"];
+                if (!empty($row["surname_prefix"])) { // check of persoon een tussenvoegsel heeft
+                    echo " " . $row["surname_prefix"];
+                }
+                echo " " . $row["surname"] . "</h3>" .
+                    "<h4>" . $date . '</h4>';
+                if (!empty($row["description"])) { // check of persoon een beschrijving heeft geplaatst bji de review
+                    echo "<p>" . $row["description"] . '</p>';
+                }
+                echo '</div>';
+                echo "<br>";
+
+            }
+            if ($ingekort) {
+                echo '<a class="reviews-ingekort" href="product-reviews.php?id=' . $id . '">Bekijk alle reviews<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
 </svg>
 </a>';
+            }
+        } else {
+            echo "Er zijn nog geen reviews voor dit product achtergelaten.";
         }
-    } else {
-        echo "Er zijn nog geen reviews voor dit product achtergelaten.";
-    }
-    $conn->close();
-    ?>
+        $conn->close();
+        ?>
+    </div>
 </div>
 
 </body>
