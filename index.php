b04d011d285cacb3f9c0a34581f3e3a487a3516b
Thomas Michiels <thomasmichiels0@gmail.com>
2023-12-08 10:40:34 +0100

    banner fix

--- index.php
+++ index.php
@@ -39,34 +39,46 @@
     <hr>
     <!-- Dynamische HTML Slider --->
 
-    <div class="slideshow-container">
-
-        <div class="mySlides fade">
-            <div class="numbertext">1 / 3</div>
-            <img src="img/slide1.jpg" style="width:100%">
-        </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext">2 / 3</div>
-            <img src="img/slide2.jpg" style="width:100%">
-        </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext">3 / 3</div>
-            <img src="img/slide3.jpg" style="width:100%">
-        </div>
-
-        <a class="prev" onclick="plusSlides(-1)">❮</a>
-        <a class="next" onclick="plusSlides(1)">❯</a>
-
+    <div class="carousel">
+        <div class="carousel-wrapper">
+            <div class="carousel-item">
+                <img src="img/slide1.jpg" alt="">
+            </div>
+            <div class="carousel-item" style="display: none;">
+                <img src="img/slide2.jpg" alt="">
+            </div>
+            <div class="carousel-item" style="display: none;">
+                <img src="img/slide3.jpg" alt="">
+            </div>
     </div>
-    <br>
-
-    <div style="text-align:center">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
-    </div>
+<!--    <div class="slideshow-container">-->
+<!---->
+<!--        <div class="mySlides fade">-->
+<!--            <div class="numbertext">1 / 3</div>-->
+<!--            <img src="img/slide1.jpg" style="width:100%">-->
+<!--        </div>-->
+<!---->
+<!--        <div class="mySlides fade">-->
+<!--            <div class="numbertext">2 / 3</div>-->
+<!--            <img src="img/slide2.jpg" style="width:100%">-->
+<!--        </div>-->
+<!---->
+<!--        <div class="mySlides fade">-->
+<!--            <div class="numbertext">3 / 3</div>-->
+<!--            <img src="img/slide3.jpg" style="width:100%">-->
+<!--        </div>-->
+<!---->
+<!--        <a class="prev" onclick="plusSlides(-1)">❮</a>-->
+<!--        <a class="next" onclick="plusSlides(1)">❯</a>-->
+<!---->
+<!--    </div>-->
+<!--    <br>-->
+<!---->
+<!--    <div style="text-align:center">-->
+<!--        <span class="dot" onclick="currentSlide(1)"></span>-->
+<!--        <span class="dot" onclick="currentSlide(2)"></span>-->
+<!--        <span class="dot" onclick="currentSlide(3)"></span>-->
+<!--    </div>-->
 
     <!-- Dynamische HTML Slider --->
     <div class="brand-introduction-container">
